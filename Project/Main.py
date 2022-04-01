233c547a573095167bc352b159539cec44553fda
Michał Cieślik <60623457+Michalcieslik1@users.noreply.github.com>
2022-04-01 16:38:19 -0400

    Began to implement the pop up functionality

--- Project/Main.py
+++ Project/Main.py
@@ -1,6 +1,8 @@
 from SoundInterface.SoundSequence import *
 from ThreadLoop import *
 from typing import Final  # Final variables
+import tkinter as tk
+from tkinter import simpledialog
 """
 Created on Wed Feb  9 20:30:01 2022
 @author: veerlemaslowski
@@ -9,6 +11,8 @@ Created on Wed Feb  9 20:30:01 2022
 Before running, in the terminal:
 - test that 'afplay /path/to/sound/' works
 TODO: Need to pick a read/write file module, threading module
+
+Tkinter tutorial: https://realpython.com/python-gui-tkinter/#getting-multiline-user-input-with-text-widgets
 """
 WAIT_MAX: Final[int] = 5
 WAIT_MIN: Final[int] = 1
@@ -16,6 +20,19 @@ SEQUENCE_QUEUE_SIZE: Final[int] = 5
 
 
 class Main:
+    pop_up = tk.Tk()
+
+    tk.Label(pop_up, text="Participant Number: ").grid(row=0)
+    tk.Label(pop_up, text="Sound Name: ").grid(row=1)
+
+    participantNum = tk.Entry(pop_up)
+    soundName = tk.Entry(pop_up)
+
+    participantNum.grid(row=0, column=1)
+    soundName.grid(row=1, column=1)
+
+    pop_up.mainloop()
+
     # Sounds used in the experiment + setup
     setupArray = ["/Users/michalcieslik/PycharmProjects/SleepStudyProject/Project/assets/Sound1.wav", "/Users/michalcieslik/PycharmProjects/SleepStudyProject/Project/assets/Sound2.wav",
                   "/Users/michalcieslik/PycharmProjects/SleepStudyProject/Project/assets/Sound3.wav"]
